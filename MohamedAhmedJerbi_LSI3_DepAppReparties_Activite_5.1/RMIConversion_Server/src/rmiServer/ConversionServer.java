df451628a8d4c62a05067ee751df43ae980a28c4
JerbiMA <m.midddou@gmail.com>
2023-11-13 15:51:16 +0100

    First rep commit

--- MohamedAhmedJerbi_LSI3_DepAppReparties_Activite_5.1/RMIConversion_Server/src/rmiServer/ConversionServer.java
+++ MohamedAhmedJerbi_LSI3_DepAppReparties_Activite_5.1/RMIConversion_Server/src/rmiServer/ConversionServer.java
@@ -1,22 +1,28 @@
 package rmiServer;
 
 import rmiService.ConversionImpl;
+import rmiService.IConversion;
 
 import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
 
-public class ConversionServer {
-    public static void main(String[] args){
+public class ConversionServer   {
+
+    public static void main(String[] args) {
 
         try {
-            LocateRegistry.createRegistry(1099);
-            ConversionImpl od = new ConversionImpl();//houni sna3na sqeulette
+            LocateRegistry.createRegistry(1095);
+            ConversionImpl od = new ConversionImpl();
             System.out.println(od.toString());
-            Naming.rebind("rmi://localhost:1099/ConversionObject",od);
+            Naming.rebind("rmi://localhost:1095/ConversionObject",od);
         } catch (RemoteException | MalformedURLException e) {
             throw new RuntimeException(e);
         }
+
+
     }
 }
--- MohamedAhmedJerbi_LSI3_DepAppReparties_Activite_5.1/RMIConversion_Server/src/rmiService/IConversion.java
+++ MohamedAhmedJerbi_LSI3_DepAppReparties_Activite_5.1/RMIConversion_Server/src/rmiService/IConversion.java
@@ -1,10 +1,9 @@
-package rmiClient;
+package rmiService;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface IConversion extends Remote{
-
+public interface IConversion extends Remote {
     double convertirMontant(double mt) throws RemoteException;
 
 }
